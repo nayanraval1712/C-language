cc23242c4acb09aa7cb68d33c8e2862794dddf5f
nayanraval1712 <ravalnayan114@gmail.com>
2023-08-21 12:52:05 +0530

    new

---
+++ 2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void main ()
+{
+    int number = 0;
+    
+    printf("number wright");
+    scanf("%d",&number);
+    
+    if (number==1)
+    {
+        printf("print jan");
+    }
+    
+    else if (number==3)
+    { printf("its March");}
+    
+    else if (number==4)
+    { printf("its April");}
+    else if (number==5)
+    { printf("its May");}
+    else if (number==6)
+    { printf("its jun");}
+    else if (number==7)
+    { printf("its Julay");}
+    else if (number==8)
+    { printf("its aug");}
+    else if (number==9)
+    { printf("its sep");}
+    else if (number==10)
+    { printf("its oct");}
+    else if (number==11)
+    { printf("its nov");}
+    else if (number==12)
+    { printf("its dec");}
+       
+        
+    }
