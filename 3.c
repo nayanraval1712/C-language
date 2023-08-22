e929a1d4fa9ff583e12f20afa61fa363d72f943c
nayanraval1712 <ravalnayan114@gmail.com>
2023-08-22 13:28:45 +0530

    ok

--- 3.c
+++ 3.c
@@ -1,49 +1,46 @@
-// finde our the bim catagery 
-#include<stdio.h>
+// finde our the bim catagery
+#include <stdio.h>
 void main()
 {
-    float weight = 0;
-    float inch =0;
-    float foot=0;
-    float meter_inch = 0;
-    float meter_foot =0;
-    float meter =0;
-    float bim =0;
- 
- printf(" print the value of weight :");
- scanf ("%f",&weight);
- printf(" print the value of foot :");
- scanf ("%f",&foot);
- printf(" print the value of inch :");
- scanf ("%f",&inch);
- 
- meter_inch = (inch / 39.37 ), meter_foot=(foot/3.281) ;
- meter = ( meter_inch + meter_foot ) ;
- printf("valur of meter%f ;", meter );
- bim = weight / (meter*meter);
- printf ("\nprint rhe bim %f; ", bim );
- 
- if (bim<16 )
- { 
-    printf("\nservise theakneass ");
-    }
- else if ( bim >16 && bim <17)
- { 
-    printf ( "\n modress thikneass");
-    }
- else if ( bim >17 && bim <18.5)
- { 
-    printf ( "\n mild  thikneass");
-    }
- else if ( bim >18.5 && bim <25)
- { 
-    printf ( "\n normal");
-    }
- else if ( bim >25 && bim <30)
- { 
-    printf ( "\n over weight ");
-    }
- 
- 
- 
+   float weight = 0;
+   float inch = 0;
+   float foot = 0;
+   float meter_inch = 0;
+   float meter_foot = 0;
+   float meter = 0;
+   float bim = 0;
+
+   printf(" print the value of weight :");
+   scanf("%f", &weight);
+   printf(" print the value of foot :");
+   scanf("%f", &foot);
+   printf(" print the value of inch :");
+   scanf("%f", &inch);
+
+   meter_inch = (inch / 39.37), meter_foot = (foot / 3.281);
+   meter = (meter_inch + meter_foot);
+   printf("valur of meter%f ;", meter);
+   bim = weight / (meter * meter);
+   printf("\nprint rhe bim %f; ", bim);
+
+   if (bim < 16)
+   {
+      printf("\nservise theakneass ");
+   }
+   else if (bim > 16 && bim < 17)
+   {
+      printf("\n modress thikneass");
+   }
+   else if (bim > 17 && bim < 18.5)
+   {
+      printf("\n mild  thikneass");
+   }
+   else if (bim > 18.5 && bim < 25)
+   {
+      printf("\n normal");
+   }
+   else if (bim > 25 && bim < 30)
+   {
+      printf("\n over weight ");
+   }
 }
---
+++ 7.c
@@ -0,0 +1,12 @@
+//   2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123 .... 3000
+#include<stdio.h>
+void main ()
+{
+    int number = 1;
+    int Answer = 0;
+    printf("%d",number);
+
+    Answer=number + 1 ;
+    printf("%d",Answer);
+}
+    
