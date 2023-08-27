623a106790c3da99e7f4e157ec67cbf00e77fd12
nayanraval1712 <ravalnayan114@gmail.com>
2023-08-27 11:11:22 +0530

    for_1

---
+++ for_1.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+void main()
+{
+    int count = 0;
+
+    for (count = 6; count > 0; count--)
+    {
+        printf("*");
+        printf("_");
+    }
+    printf("\n");
+    printf(" ");
+    // printf("*");
+    // printf("_");
+    // printf("*");
+    // printf("_");
+    // printf("*");
+    // printf("_");
+    // printf("*");
+    // printf("_");
+    // printf("*");
+    // printf("_");
+    for (count = 5; count > 0; count--)
+    {
+        printf("*");
+        printf("_");
+        // printf("*");
+        // printf("_");
+        // printf("*");
+        // printf("_");
+        // printf("*");
+        // printf("_");
+        // printf("*");
+        // printf("_");
+    }
+    printf("\n");
+    printf(" ");
+    printf(" ");
+    for (count = 4; count > 0; count--)
+    {
+        printf("*");
+        printf("_");
+    }
+    printf("\n");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    for (count = 3; count > 0; count--)
+    {
+        printf("*");
+        printf("_");
+    }
+    printf("\n");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    for (count = 3; count > 0; count--)
+    {
+        printf("*");
+        printf("_");
+    }
+    printf("\n");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    for (count = 2; count > 0; count--)
+    {
+        printf("*");
+        printf("_");
+    }
+    printf("\n");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+    printf(" ");
+     for (count = 1; count > 0; count--)
+    {
+        printf("*");
+        printf("_");
+    }
+}
